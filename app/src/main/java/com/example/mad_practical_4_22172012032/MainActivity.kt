a2e263e2cdbee15aaf32d25767b3ed5859948a56
surekhabhanvar123 <surekhabenbhanvar12@gmail.com>
2023-09-13 21:27:33 +0530

    h

--- app/src/main/java/com/example/mad_practical_4_22172012032/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22172012032/MainActivity.kt
@@ -1,11 +1,23 @@
-package com.example.mad_practical_4_22172012032
+package com.example.mad_practical_4_22172012003
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button =findViewById<View>(R.id.button3)
+        var button2 =findViewById<View>(R.id.button4)
+        button.setOnClickListener {
+            intent = Intent(this, Login_Activity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener{
+            intent = Intent(this, Registration_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/mad_practical_4_22172012032/Registration.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical_4_22172012003
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
